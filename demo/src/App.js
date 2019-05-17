139aed9c73b0c547958023054cb15f7698ecee80
Steven VanWoerkom <vanwoerkom.steve@gmail.com>
2019-05-16 22:14:06 -0400

    removed example

--- demo/src/App.js
+++ demo/src/App.js
@@ -1,19 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
-import SimpleNav from './components/SimpleNav';
-import BooleanShort from './components/BooleanShort';
-import RandomUser from './components/RandomUser';
-import Swanson from './components/Swanson';
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <h1>Conditional Rendering Demo</h1>
-        <RandomUser />
-        <BooleanShort />
-        <Swanson />
-        <SimpleNav />
+        
       </div>
     );
   }
